c80b46fbe2133c7c07b3bd5915e69815e3dd4377
datsun80zx <m.richardp93@gmail.com>
2025-04-13 10:10:05 -0400

    added in basic REPL functionality

--- repl.go
+++ repl.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+
+	// so i need to set up a new scanner
+	userInput := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		userInput.Scan()
+		words := cleanInput(userInput.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(strings.TrimSpace(text))
