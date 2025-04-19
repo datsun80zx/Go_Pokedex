09b4d390760fa5f5ed447d97045e7aeb85fc60f1
datsun80zx <m.richardp93@gmail.com>
2025-04-19 09:19:39 -0400

    refactored code base to make it more modular

--- repl.go
+++ repl.go
@@ -7,30 +7,47 @@ import (
 	"strings"
 )
 
+func startRepl() {
+	userInput := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		userInput.Scan()
+
+		words := cleanInput(userInput.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	lowered := strings.ToLower(strings.TrimSpace(text))
+	words := strings.Fields(lowered)
+	return words
+}
+
 type cliCommand struct {
 	name        string
 	description string
 	callback    func() error
 }
 
-var registry map[string]cliCommand
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n\n")
-	for key, value := range registry {
-		fmt.Printf("%s: %s\n", key, value.description)
-	}
-	return nil
-}
-
-func init() {
-	registry = map[string]cliCommand{
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
@@ -41,38 +58,12 @@ func init() {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		// "map": {
+		// 	name:        "help", 
+		// 	description: "Displays the names of 20 location areas in the Pokemon world",
+		// 	callback:	 commandMap,
+		// },
 	}
 }
 
-func startRepl() {
 
-	// so i need to set up a new scanner
-	userInput := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		userInput.Scan()
-		words := cleanInput(userInput.Text())
-		if len(words) == 0 {
-			continue
-		}
-
-		commandName := words[0]
-
-		command, exists := registry[commandName]
-		if exists {
-			err := command.callback()
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			fmt.Println("Unknown command")
-		}
-	}
-}
-
-func cleanInput(text string) []string {
-	lowered := strings.ToLower(strings.TrimSpace(text))
-	words := strings.Fields(lowered)
-	return words
-}
